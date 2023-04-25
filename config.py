f4913d9d1512638745b2152c8a3b37f47bbb0b5c
mertemr <48881002+mertemr@users.noreply.github.com>
2023-04-25 13:38:30 +0300

    Config File
    Signed-off-by: mertemr <48881002+mertemr@users.noreply.github.com>

---
+++ config.py
@@ -0,0 +1,21 @@
+import machine
+
+class PINS:
+    D0 = 16
+    D1 = 5
+    D2 = 4
+    D3 = 0
+    D4 = 2
+    D5 = 14
+    D6 = 12
+    D7 = 13
+    D8 = 15
+
+OUT = machine.Pin.OUT
+
+WIFI_SSID = "YOUR_WIFI_SSID"
+WIFI_PASSWORD = "YOUR_WIFI_PASSWORD"
+
+status_led = machine.Pin(PINS.D0, OUT)      # Blink when running
+wifi_led   = machine.Pin(PINS.D1, OUT)      # Light up when connected to WiFi
+wake_on_lan_pin = machine.Pin(PINS.D2, OUT)
